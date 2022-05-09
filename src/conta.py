4a06c2666f7b91bff12dd4fc8bd90e93e6aca647
vitor martini <vitor-martini@outlook.com>
2022-05-09 13:15:51 -0300

    Desenvolvidos métodos necessários

--- src/conta.py
+++ src/conta.py
@@ -1,16 +1,36 @@
 class Conta():
     def __init__(self, numero, titular, saldo, limite = 1000):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite 
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite 
 
-    def extrato(self):
-        return self.saldo
+    @property
+    def saldo(self):
+        return self.__saldo
+    
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, valor):
+        self.__limite = valor
 
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
+
+    def eh_saldo_negativo(self, valor):
+        return self.__saldo < valor
 
     def sacar(self, valor):
-        self.saldo -= valor
+        if self.eh_saldo_negativo(valor):
+            raise Exception('Saldo indisponível')
+        self.__saldo -= valor
 
+    def transferir(self, valor, conta_destino):
+        self.sacar(valor)
+        conta_destino.depositar(valor)
+
+    def extrato(self):
+        print('O saldo da conta "{}" do titular "{}" é de: R${}'.format(self.__numero, self.__titular, self.__saldo))
