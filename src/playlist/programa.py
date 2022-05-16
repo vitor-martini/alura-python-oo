7f78c6cda8ccc78aacfca5a5426632bb48487fdc
vitor martini <vitor-martini@outlook.com>
2022-05-15 21:07:55 -0300

    polimorfismo

--- src/playlist/programa.py
+++ src/playlist/programa.py
@@ -19,14 +19,35 @@ class Programa():
     def nome(self, nome):
         self.__nome = nome
 
+    def __str__(self):
+        return f'{self.__nome} - {self.ano} - {self.__likes}'
+
 class Filme(Programa):
     def __init__(self, nome, ano, likes, duracao):
         super().__init__(nome, ano, likes)
-        self.duracao = duracao
+        self.__duracao = duracao
+
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.__duracao}min - {self.likes} likes'
 
 class Serie(Programa):
     def __init__(self, nome, ano, likes, temporada):
         super().__init__(nome, ano, likes)
-        self.temporada = temporada
+        self.__temporada = temporada
 
-    
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.__temporada} temporada(s) - {self.likes} likes'
+
+    def __repr__(self):
+        return f"Serie('{self.nome}', {self.ano}, {self.likes}, {self.__temporada})"
+        
+class Playlist:
+    def __init__(self, nome, programas):
+        self.__nome = nome
+        self.programas = programas 
+
+    def __getitem__(self, item):
+        return self.programas[item]
+
+    def __len__(self):
+        return len(self.programas)
--- test/test_playlist_programa.py
+++ test/test_playlist_programa.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from winsound import PlaySound
 import pytest
 
 source_path = os.path.dirname(os.path.abspath("programa.py")) + '\\src\\playlist'
@@ -7,6 +8,7 @@ sys.path.insert(1, source_path)
 from programa import Programa 
 from programa import Filme
 from programa import Serie
+from programa import Playlist
 
 def criar_filme():
     filme = Filme('Vingadores', 2012, 0, 160)   
@@ -15,6 +17,13 @@ def criar_filme():
 def criar_serie():
     serie = Serie('Cavaleiro da Lua', 2022, 0, 1)
     return serie
+
+def criar_playlist():
+    filme = criar_filme()
+    serie = criar_serie()
+    programas = [filme, serie]
+    playlist = Playlist('Programas da Marvel', programas)
+    return playlist
 
 def test_nome():
     filme = criar_filme()
@@ -25,3 +34,33 @@ def test_likes():
     filme = criar_filme()
     filme.dar_likes(3)
     assert filme.likes == 3
+
+def test_str():
+    filme = criar_filme()    
+    assert str(filme) == 'Vingadores - 2012 - 160min - 0 likes'
+
+def test_polimorfismo():
+    filme = criar_filme()
+    serie = criar_serie()
+    
+    playlist = [filme, serie]
+    str_filme = 'Vingadores - 2012 - 160min - 0 likes'
+    str_serie = 'Cavaleiro da Lua - 2022 - 1 temporada(s) - 0 likes'
+    for programa in playlist:
+        assert str(programa) == str_filme if type(programa) == Filme else str_serie
+
+def test_repr():
+    serie = criar_serie()    
+    assert repr(serie) == "Serie('Cavaleiro da Lua', 2022, 0, 1)"
+
+def test_iteracao():
+    playlist = criar_playlist()
+    str_filme = 'Vingadores - 2012 - 160min - 0 likes'
+    str_serie = 'Cavaleiro da Lua - 2022 - 1 temporada(s) - 0 likes'
+    for programa in playlist:
+        assert str(programa) == str_filme if type(programa) == Filme else str_serie
+
+def test_tamanho():
+    playlist = criar_playlist()
+    assert len(playlist) == 2
+    
